3dc091bc1a70e8a7e1b14ba565d9c4e9b656fa11
Ajtene Kurtaliqi <ajtene.kurtaliqi@gmail.com>
2026-04-01 22:32:08 +0200

    [backend] implement get all files and download file by id

--- backend/src/main/java/com/praxedo/securefiles/domain/FileMetaDataRepository.java
+++ backend/src/main/java/com/praxedo/securefiles/domain/FileMetaDataRepository.java
@@ -1,5 +1,10 @@
 package com.praxedo.securefiles.domain;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FileMetaDataRepository {
     FileMetaData save(FileMetaData fileMetaData);
+    List<FileMetaData> findAll();
+    Optional<FileMetaData> findById(Long id);
 }
